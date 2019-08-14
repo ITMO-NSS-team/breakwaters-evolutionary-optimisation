7806c0cddfcc6018877f1582e7fb6740a0a596d4
Nikolay Nikitin <nicl.nno@gmail.com>
2019-08-14 22:48:17 +0300

    Name corrected for manual modifications list

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from Classes.WaveModel import SimpleGeomWaveModel, WaveSimulationResult
 
 exp_domain = SochiHarbor()
 
-all_modifications = []
+manual_modifications = []
 
-all_modifications = {
+manual_modifications = {
     "mod1": Breaker_descr(list(map(xy_to_points, [[30, 20], [33, 22], [42, 17]])), 0, 'Ia'),
     "mod2": Breaker_descr(list(map(xy_to_points, [[24, 16], [33, 22], [42, 17]])), 0, 'Ia'),
     "mod12": Breaker_descr(list(map(xy_to_points, [[50, 24], [50, 32], [50, 39]])), 0, 'II'),
     "mod26": Breaker_descr(list(map(xy_to_points, [[56, 40], [56, 38]])), 0, '-'),
 }
 
-breaker_modification_1 = Breaker("mod1", all_modifications["mod1"])
-breaker_modification_12 = Breaker("mod12", all_modifications["mod12"])
-breaker_modification_26 = Breaker("mod26", all_modifications["mod26"])
+breaker_modification_1 = Breaker("mod1", manual_modifications["mod1"])
+breaker_modification_12 = Breaker("mod12", manual_modifications["mod12"])
+breaker_modification_26 = Breaker("mod26", manual_modifications["mod26"])
 
 modifications = [breaker_modification_1, breaker_modification_12, breaker_modification_26]
 
