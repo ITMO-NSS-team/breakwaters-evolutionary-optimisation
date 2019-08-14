5e191de69770235a8cb6b0a165b159e4c8aee436
Nikolay Nikitin <nicl.nno@gmail.com>
2019-08-15 01:29:02 +0300

    Folder structure updated

---
+++ Simulation/ModelingStrategies.py
@@ -0,0 +1,32 @@
+import abc
+import numpy as np
+import os
+from Simulation.Results import WaveSimulationResult
+
+
+class SimulationStrategyAbstract(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def simulate(self, configuration_info):
+        return
+
+
+class SimpleGeomSimulationStrategy(SimulationStrategyAbstract):
+    def simulate(self, configuration_info):
+        hs = np.zeros(shape=(configuration_info.domain.model_grid.grid_x, configuration_info.domain.model_grid.grid_y))
+
+        # represent constructions
+
+        # do math
+
+        return WaveSimulationResult(hs)
+
+
+class SwanSimulationStrategy(SimulationStrategyAbstract):
+    def simulate(self, configuration_info):
+        os.system(r"swanrun.bat {}".format(configuration_info.info))
+
+        hs = np.zeros(shape=(configuration_info.model_grid.grid_x, configuration_info.model_grid.grid_y))
+
+        return hs
