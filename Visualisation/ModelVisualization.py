Corrected 5% height calculation Experiments and it's visualisation modified

Visualisation/ModelVisualization.py
@@ -98,10 +98,11 @@ class ModelsVisualization:
         plt.close('all')
 
     def experimental_visualise(self, hs: np.ndarray, all_breakers, base_breakers, fairways, target_points,
-                               title_mod, vmax, order_id, is_wind, rep_info, dir_info, real_ang):
+                               title_mod, vmax, order_id, is_wind, rep_info, dir_info, real_ang, len_info):
 
+        plt.rcParams['figure.figsize'] = [7, 5]
+        plt.rcParams["font.size"] = "8"
         fig = plt.figure()
-        plt.rcParams['figure.figsize'] = [15, 10]
         ax = plt.subplot()
         ax.axes.set_aspect('equal')
 
@@ -114,7 +115,7 @@ class ModelsVisualization:
             wind_str = "с учетом локального ветра."
 
         ax.set_title(
-            f'{order_id} Высоты волн с {title_mod}% обеспеченностью в целевых точках: {values} м. \n Направление волнения {dir_info} для повторяемости раз в  {rep_info} лет, {wind_str}')
+            f'{order_id} Высоты волн с {title_mod}% обеспеченностью в целевых точках: {values} м. \n Направление волнения {dir_info} для повторяемости раз в  {rep_info} лет,\n {wind_str} Длины доп. сооружений:\n {len_info}')
 
         map_of_place = hs
 
@@ -140,16 +141,16 @@ class ModelsVisualization:
 
                 if [all_breakers[i].points[j - 1].x, all_breakers[i].points[j - 1].y] not in breaker_points:
                     breaker_points.append([all_breakers[i].points[j - 1].x, all_breakers[i].points[j - 1].y])
-                    plt.annotate(
-                        f'({all_breakers[i].points[j-1].x},{all_breakers[i].points[j-1].y})',
-                        (all_breakers[i].points[j - 1].x, all_breakers[i].points[j - 1].y))
+                    #plt.annotate(
+                    #    f'({all_breakers[i].points[j-1].x},{all_breakers[i].points[j-1].y})',
+                    #    (all_breakers[i].points[j - 1].x, all_breakers[i].points[j - 1].y))
 
                 if j == len(all_breakers[i].points) - 1:
                     if [all_breakers[i].points[j].x, all_breakers[i].points[j].y] not in breaker_points:
                         breaker_points.append([all_breakers[i].points[j].x, all_breakers[i].points[j].y])
-                        plt.annotate(
-                            f'({all_breakers[i].points[j].x},{all_breakers[i].points[j].y})',
-                            (all_breakers[i].points[j].x, all_breakers[i].points[j].y))
+                        #plt.annotate(
+                        #    f'({all_breakers[i].points[j].x},{all_breakers[i].points[j].y})',
+                        #    (all_breakers[i].points[j].x, all_breakers[i].points[j].y))
 
         for i in range(len(base_breakers)):
             for j in range(1, len(base_breakers[i].points)):
@@ -162,9 +163,7 @@ class ModelsVisualization:
             plt.plot(p1, p2, '--', c='g', linewidth=2, marker='.')
             # print(self.fairways[0].x1)
 
-        for point_ind, point in enumerate(target_points):
-            plt.scatter(point.x, point.y, color='black', marker='o')
-            plt.annotate(f'[№{point_ind},{point.x+2},{point.y+2}]', (point.x, point.y), color='black')
+        #
 
         wind_names = ["С", "СВ", "В", "ЮВ", "Ю", "ЮЗ", "З", "СЗ"]
 
@@ -172,21 +171,21 @@ class ModelsVisualization:
         base_y = 9
         k = 5
         for ang_ind, ang in enumerate([0, 45, 90, 135, 180, 225, 270, 315]):
-            ang2 = (ang + 120) % 360
+            ang2 = (ang+120) % 360
             new_x = np.sin(ang2 / 180 * math.pi) * k
-            new_y = np.cos(ang2 / 180 * math.pi) * k
+            new_y = -np.cos(ang2 / 180 * math.pi) * k
 
             plt.plot([base_x, base_x + new_x], [base_y, base_x + new_y], color="black")
 
             new_x = np.sin(ang2 / 180 * math.pi) * (k + 2)
-            new_y = np.cos(ang2 / 180 * math.pi) * (k + 2)
+            new_y = -np.cos(ang2 / 180 * math.pi) * (k + 2)
 
             plt.annotate(wind_names[ang_ind], (base_x + new_x, base_y + new_y), color='black')
 
-        real_ang = (real_ang + 120 + 180) % 360
+        real_ang = (real_ang + 120 + 180 ) % 360
 
         new_x = np.sin(real_ang / 180 * math.pi) * (k + 2)
-        new_y = np.cos(real_ang / 180 * math.pi) * (k + 2)
+        new_y = -np.cos(real_ang / 180 * math.pi) * (k + 2)
 
         plt.arrow(base_x, base_y, new_x, new_y, length_includes_head=True,
                   head_width=0.5, head_length=0.5, color="cyan", width=0.2, zorder=10)
@@ -194,7 +193,6 @@ class ModelsVisualization:
         # plt.figure(figsize=(4, 5))
         if not os.path.isdir(f'img/experiments/{self.exp_name}'):
             os.mkdir(f'img/experiments/{self.exp_name}')
-        # plt.set_size_inches(4.1,2.9)
         plt.savefig(f'img/experiments/{self.exp_name}/{self.configuration_label}.png', bbox_inches='tight')
         # plt.show()
         plt.clf()