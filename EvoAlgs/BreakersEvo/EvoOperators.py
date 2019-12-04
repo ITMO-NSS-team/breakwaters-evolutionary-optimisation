Fixed bug with default item calculation

EvoAlgs/BreakersEvo/EvoOperators.py
@@ -1,27 +1,15 @@
-import copy
-import itertools
-import os
 import random
 import uuid
-from itertools import chain
 import copy
 
 import numpy as np
-from pyDOE import lhs
-from scipy.stats.distributions import norm
 
-from Breakers.BreakersUtils import BreakersUtils
 from CommonUtils.StaticStorage import StaticStorage
-from EvoAlgs.BreakersEvo.BreakersEvoUtils import BreakersEvoUtils
 from EvoAlgs.BreakersEvo.BreakerStructureRepresentation import BreakerStructureRepresentation
 from EvoAlgs.EvoAnalytics import EvoAnalytics
-from Optimisation.Objective import ObjectiveData, NavigationObjective, StructuralObjective, ConstraintComparisonType
+from Optimisation.Objective import ObjectiveData, ConstraintComparisonType
 from Visualisation.ModelVisualization import ModelsVisualization
 from Simulation.Results import WaveSimulationResult
-
-# TODO refactor
-len_range = [0, 3]
-dir_range = [-50, 50]
 
 
 def _flatten(items, seqtypes=(list, tuple)):
@@ -32,7 +20,10 @@ def _flatten(items, seqtypes=(list, tuple)):
 
 
 def calculate_objectives(model, task, population, visualiser=None):
-    pre_simulated_results = model.computational_manager.prepare_simulations_for_population(population, model)
+    if any(obj.is_simulation_required for obj in task.objectives):
+        pre_simulated_results = model.computational_manager.prepare_simulations_for_population(population, model)
+    else:
+        pre_simulated_results = None
 
     for individ_index, individual in enumerate(population):
         proposed_breakers = individual.genotype.get_genotype_as_breakers()

OptRuns/paper_exp/ExperimentalEnvironment.py
@@ -1,15 +1,12 @@
 import datetime
 import random
 from builtins import staticmethod
-from enum import Enum
 
-import numpy as np
-
-from Breakers.Breaker import xy_to_points, Breaker
+from Breakers.Breaker import xy_to_points
 from CommonUtils.StaticStorage import StaticStorage
 from Computation.СomputationalEnvironment import SwanWinRemoteComputationalManager, SwanWinLocalComputationalManager
 from Configuration.Domains import SochiHarbor
-from EvoAlgs.BreakersEvo.GenotypeEncoders import AngularGenotypeEncoder, CartesianGenotypeEncoder
+from EvoAlgs.BreakersEvo.GenotypeEncoders.GenotypeEncoder import AngularGenotypeEncoder, CartesianGenotypeEncoder
 from EvoAlgs.EvoAnalytics import EvoAnalytics
 from Optimisation.Objective import *
 from Optimisation.OptimisationTask import OptimisationTask
@@ -151,3 +148,59 @@ class ExperimentalEnvironment:
             visualiser = Visualiser(vis_settings, vis_data)
 
             optimiser.optimise(wave_model, task, visualiser=visualiser)
+
+class TestEnvironment(ExperimentalEnvironment):
+    def run_optimisation_experiment(self, task_id, enc_id, algopt_id, run_local):
+        if __name__ == 'OptRuns.paper_exp.ExperimentalEnvironment':
+            np.random.seed(self.seed)
+            random.seed(self.seed)
+
+            exp_domain = SochiHarbor()
+
+            ExperimentalEnvironment._init_conditions_for_experiment(exp_domain)
+
+            exp_name = f"test_{algopt_id}_task{task_id}_enc{enc_id}"
+            EvoAnalytics.clear()
+            EvoAnalytics.run_id = 'run_{exp_name}_{date:%Y_%m_%d_%H_%M_%S}'.format(exp_name=exp_name,
+                                                                                   date=datetime.datetime.now())
+            if run_local:
+                computational_manager = SwanWinLocalComputationalManager()
+            else:
+                computational_manager = SwanWinRemoteComputationalManager(resources_names=["125", "124", "123", "121"])
+            wave_model = SwanWaveModel(exp_domain, computational_manager)
+            wave_model.model_results_file_name = 'D:\SWAN_sochi\model_results_paper_martech.db'
+
+            optimiser = ExperimentalEnvironment._get_optimiser_for_experiment(algopt_id)
+
+            selected_modifications_for_tuning = ExperimentalEnvironment._get_modifications_for_experiment(task_id)
+
+            optimisation_objectives = [
+                RelativeNavigationObjective(),
+                RelativeCostObjective()]
+
+            analytics_objectives = [
+                CostObjective,
+                NavigationObjective,
+                WaveHeightObjective,
+                RelativeQuailityObjective()]
+
+            task = OptimisationTask(optimisation_objectives, selected_modifications_for_tuning,
+                                    goal="minimise")
+            task.constraints = [(StructuralObjective(), ConstraintComparisonType.equal, 0)]
+
+            StaticStorage.task = task
+
+            StaticStorage.genotype_encoder = ExperimentalEnvironment._get_encoder_for_experiment(enc_id)
+
+            vis_settings = VisualisationSettings(store_all_individuals=False, store_best_individuals=True,
+                                                 num_of_best_individuals_from_population_for_print=5,
+                                                 create_gif_image=True,
+                                                 create_boxplots=True,
+                                                 print_pareto_front=True)
+            vis_data = VisualisationData(optimisation_objectives, base_breakers=exp_domain.base_breakers, task=task)
+
+            visualiser = Visualiser(vis_settings, vis_data)
+
+            opt_res = optimiser.optimise(wave_model, task, visualiser=visualiser)
+            print("Final result")
+            print(opt_res.history[19][0].genotype.get_parameterized_chromosome_as_num_list())