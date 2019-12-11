014c13652a882a39bf80da13d94448cef4ed37e0
Nikolay Nikitin <nicl.nno@gmail.com>
2019-12-11 22:00:02 +0300

    Missing argument fixed

--- EvoAlgs/BreakersEvo/EvoOperators.py
+++ EvoAlgs/BreakersEvo/EvoOperators.py
@@ -89,7 +89,7 @@ def calculate_objectives(model, task, population, visualiser=None):
         local_id = local_id + 1
 
     if visualiser is not None:
-        visualiser.print_individuals(population, fitnesses=None)
+        visualiser.print_individuals(population)
 
 
 def crossover(p1, p2, rate):
